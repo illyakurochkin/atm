43126bfbac86b0251825eb595a1e9aa9d1f77f13
illyakurochkin <illya.kurochkin@gmail.com>
2019-11-30 19:35:04 +0200

    Without hooks

--- src/screens/CreateTransaction/CreateTransactionForm.js
+++ src/screens/CreateTransaction/CreateTransactionForm.js
@@ -1,41 +1,27 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {connect} from 'react-redux';
 import {Field, formValueSelector, getFormSyncErrors, reduxForm} from 'redux-form';
 import {Form} from 'semantic-ui-react';
 import {normalizeAmount, normalizeCard} from '../../imports/utils/forms/normalizers';
 import {validateAmount, validateReceiverCard} from '../../imports/utils/forms/validators';
 import {selectAccount} from '../../imports/ducks/account';
 
-const form = 'createTransaction';
-const selector = formValueSelector(form);
-
-const mapStateToProps = state => ({
-  myCard: selectAccount(state).card,
-  amount: selector(state, 'amount'),
-  receiverCard: selector(state, 'receiverCard'),
-  errors: Object.values(getFormSyncErrors(form)(state)),
-});
-
-const CreateTransactionForm = ({handleSubmit}) => {
-  const {myCard, amount, receiverCard, errors} = useSelector(mapStateToProps);
-
-  return (
-    <Form inverted onSubmit={handleSubmit} error={errors.length}>
-      <Field
-        name="receiverCard"
-        component={renderReceiverCardField}
-        normalize={normalizeCard(receiverCard)}
-        validate={validateReceiverCard(myCard)}
-      />
-      <Field
-        name="amount"
-        component={renderAmountField}
-        normalize={normalizeAmount(amount)}
-        validate={validateAmount}
-      />
-    </Form>
-  );
-};
+const CreateTransactionForm = ({handleSubmit, myCard, amount, receiverCard, errors}) => (
+  <Form inverted onSubmit={handleSubmit} error={errors.length}>
+    <Field
+      name="receiverCard"
+      component={renderReceiverCardField}
+      normalize={normalizeCard(receiverCard)}
+      validate={validateReceiverCard(myCard)}
+    />
+    <Field
+      name="amount"
+      component={renderAmountField}
+      normalize={normalizeAmount(amount)}
+      validate={validateAmount}
+    />
+  </Form>
+);
 
 const renderReceiverCardField = (field) => (
   <Form.Field>
@@ -51,5 +37,17 @@ const renderAmountField = (field) => (
   </Form.Field>
 );
 
-export default reduxForm({form})(CreateTransactionForm);
+const form = 'createTransaction';
+const selector = formValueSelector(form);
+
+const mapStateToProps = state => ({
+  myCard: selectAccount(state).card,
+  amount: selector(state, 'amount'),
+  receiverCard: selector(state, 'receiverCard'),
+  errors: Object.values(getFormSyncErrors(form)(state)),
+});
+
+const Connected = connect(mapStateToProps)(CreateTransactionForm);
+
+export default reduxForm({form})(Connected);
 
